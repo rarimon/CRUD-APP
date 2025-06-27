e8b3ce17c7b9fe9f3a6f12518b0c4529893efc28
rarimon <aminulislam221@gmail.com>
2025-06-27 22:49:50 +0600

    added profile page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {Profiler} from 'react';
 import MainLayout from "./layouts/MainLayout.jsx";
 import {BrowserRouter, Route, Routes} from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
+                <Route path="/profile" element={<ProfilePage/>} />
             </Routes>
 
         </BrowserRouter>
